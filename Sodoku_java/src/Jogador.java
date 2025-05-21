e9aa5e564e81da1ca9a222617faef0b6b853a768
Jose Heuwald <jcheuwald@hotmail.com>
2025-05-20 22:44:32 -0300

    feat: Melhoria para não permitir informar valor inválido.

--- Sodoku_java/src/Jogador.java
+++ Sodoku_java/src/Jogador.java
@@ -34,17 +34,39 @@ public class Jogador {
     }
 
     public int obterLinha() {
-        System.out.print("Digite a linha (0-8): ");
-        return scanner.nextInt();
+        return Integer.parseInt(obterTexto("Digite a linha (0-8)", "[0-8]"));
     }
 
     public int obterColuna() {
-        System.out.print("Digite a coluna (0-8): ");
-        return scanner.nextInt();
+        return Integer.parseInt(obterTexto("Digite a linha (0-8)", "[0-8]"));
     }
 
     public int obterValor() {
-        System.out.print("Digite o valor (1-9): ");
-        return scanner.nextInt();
+        return Integer.parseInt(obterTexto("Digite o valor (1-9)", "[0-9]"));
+    }
+    
+    private String obterTexto(String titulo, String regex) {
+    	
+    	int count = 0;
+    	
+    	String valor = "";
+    	
+    	do {
+    		
+    		System.out.print(titulo + ": ");
+    		
+    		valor = scanner.next();
+    		
+    		if(valor != null && valor.matches(regex)) {
+    			return valor;
+    		}
+    		System.out.println("Valor digitado inválido.");
+		} while (count++ < 10);
+    	
+    	System.out.println();
+    	System.out.println("Ta de sacanagem.");
+    	System.out.println("Encerrando aplicação.");
+    	System.exit(0);
+    	return valor;
     }
 }
